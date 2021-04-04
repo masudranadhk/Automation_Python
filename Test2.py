ed814a0b55d08a88ff174bfa93029e3720ed6790
masudranadhk <56155585+masudranadhk@users.noreply.github.com>
2021-04-04 14:33:04 +0600

    Update Test2.py

--- Test2.py
+++ Test2.py
@@ -1,27 +1,26 @@
 # By assigning only column
-
 print("Enter your value:", end=" ")
 a = int(input())
-b=0
-#print("Row value:", end=" ")
-#b = int(input())
-row = 0
-column = 0
-while row <= a:
-        while column <= b:
-            print("*", end=" ")
-            column += 1
-        row += 1
-        if row > 1:
-            column = 0
-            b += 1
-            print()
+for i in range(a):
+    for j in range(i+1):
+        print("*", end=" ")
+    print()
 
-# # The program can be code as below also
-# c = '*'
+# # Another way
+# # By assigning only column
 # print("Enter your value:", end=" ")
-# width = int(input())
-# for i in range  (0,width+1):
-#     print(c*i, end=" ")
-#     i += 1
-#     print()
+# a = int(input())
+# b=0
+# #print("Row value:", end=" ")
+# #b = int(input())
+# row = 0
+# column = 0
+# while row <= a:
+#         while column <= b:
+#             print("*", end=" ")
+#             column += 1
+#         row += 1
+#         if row > 1:
+#             column = 0
+#             b += 1
+#             print()
